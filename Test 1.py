a2f314dc9bfe7c39bd1b3f474f77178d7ce28f4a
Fraserj89 <jamiefraser1989@googlemail.com>
2017-09-30 11:15:55 +0100

    Update Test 1.py
    Marking the answers

--- Test 1.py
+++ Test 1.py
@@ -1,2 +1,5 @@
+# setting the variable is correct :) below
 person = input("enter your name: ")
+
+# however here it will actually print 'Hello, person', rather than the persons name.  The variable is not being called whilst in ' '.
 print('Hello, person')
