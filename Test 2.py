dba2300607654d44d2d0e8c90f4f652e1bb61d14
JackMersey <32368083+JackMersey@users.noreply.github.com>
2017-10-02 15:14:15 +0100

    Add files via upload

--- Test 2.py
+++ Test 2.py
@@ -1,4 +1,4 @@
-person = input("enter your name: ")
-print 'Hello,' + person
-Job = input("So what do you do for a living?: ")
-print = "I see your a" + Job
+person = input("enter your name: ") 
+print "Hello," + person 
+Job = input("So what do you do for a living?: ") 
+print = "I see your a" + Job 
